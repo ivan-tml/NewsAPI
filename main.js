c3ed0551760c125be8d4205ee0c9ec0fbca83d3c
Ivan Tomljenovic <tomljenovic21@gmail.com>
2020-04-28 21:14:58 +0000

    Added styling and and functionality

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-let xhr = new XMLHttpRequest();
-let news = "";
+function getNews(category, jsonNews) {
+  let xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://newsapi.org/v2/top-headlines?country=ie&apiKey=524728793aad4f8089ebe35505369da4")
-xhr.send();
+  xhr.open(
+    "GET",
+    "https://newsapi.org/v2/top-headlines?country=ie&" +
+      category +
+      "apiKey=524728793aad4f8089ebe35505369da4"
+  );
+  xhr.send();
 
-function getNews(jsonNews) {
-    news = jsonNews;
-    
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      jsonNews(JSON.parse(this.responseText));
+    }
+  };
 }
 
-xhr.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-        news = this.responseText;
-        getNews(JSON.parse(this.responseText));
-    }
-};
-
+function writeToDocument(category) {
+  let el = document.getElementById("data");
+  el.innerHTML = "";
+  getNews(category, function(data) {
+    data = data.articles;
+    data.slice(0, 10).forEach(function(item) {
+      el.innerHTML += "<div class='newsBox'><h4>" + item.title + "</h4>" + "<br />" + "<p>" + item.content + "</p></div>";
+    });
+  });
+}
